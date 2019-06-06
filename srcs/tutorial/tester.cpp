dc8b48f19744edd3cf3f709be9d7df019a17803c
welcometothegalaxy <iamjam4944@gmail.com>
2019-06-07 02:51:12 +0900

    first commit

---
+++ srcs/tutorial/tester.cpp
@@ -0,0 +1,14 @@
+//this is only for CMake tutorial
+
+#include <iostream>
+
+int main(){
+
+    std::cout<< "Hello CMake!"<<std::endl;
+
+	std::cout << "Does this change the source only?" << std::endl;
+
+	//third level?
+
+    return 0;
+}
