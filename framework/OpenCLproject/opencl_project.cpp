cc0e182df959e377f0cba93ac8e4d13c301b51f2
welcometothegalaxy <iamjam4944@gmail.com>
2019-07-06 08:37:10 +0900

    add link_opencl

---
+++ framework/OpenCLproject/opencl_project.cpp
@@ -0,0 +1,98 @@
+//test link OpenCL
+
+#include "CL/cl.hpp"
+
+int main()
+{
+	int i, j;
+	char* value;
+	size_t valueSize;
+	cl_uint platformCount;
+	cl_platform_id* platforms;
+	cl_uint deviceCount;
+	cl_device_id* devices;
+	cl_uint maxComputeUnits;
+
+	// get all platforms
+	clGetPlatformIDs(0, NULL, &platformCount);
+	platforms = (cl_platform_id*)malloc(sizeof(cl_platform_id) * platformCount);
+	clGetPlatformIDs(platformCount, platforms, NULL);
+
+	for (i = 0; i < platformCount; i++) {
+
+		// get all devices
+		clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, NULL, &deviceCount);
+		devices = (cl_device_id*)malloc(sizeof(cl_device_id) * deviceCount);
+		clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, deviceCount, devices, NULL);
+
+		// for each device print critical attributes
+		for (j = 0; j < deviceCount; j++) {
+
+			// print device name
+			clGetDeviceInfo(devices[j], CL_DEVICE_NAME, 0, NULL, &valueSize);
+			value = (char*)malloc(valueSize);
+			clGetDeviceInfo(devices[j], CL_DEVICE_NAME, valueSize, value, NULL);
+			printf("platform %d. Device %d: %s\n", i + 1, j + 1, value);
+			free(value);
+
+			// print hardware device version
+			clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, 0, NULL, &valueSize);
+			value = (char*)malloc(valueSize);
+			clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, valueSize, value, NULL);
+			printf(" %d.%d Hardware version: %s\n", i + 1, 1, value);
+			free(value);
+
+			// print software driver version
+			clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, 0, NULL, &valueSize);
+			value = (char*)malloc(valueSize);
+			clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, valueSize, value, NULL);
+			printf(" %d.%d Software version: %s\n", i + 1, 2, value);
+			free(value);
+
+			// print c version supported by compiler for device
+			clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, 0, NULL, &valueSize);
+			value = (char*)malloc(valueSize);
+			clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, valueSize, value, NULL);
+			printf(" %d.%d OpenCL C version: %s\n", i + 1, 3, value);
+			free(value);
+
+			// print parallel compute units
+			clGetDeviceInfo(devices[j], CL_DEVICE_MAX_COMPUTE_UNITS,
+				sizeof(maxComputeUnits), &maxComputeUnits, NULL);
+			printf(" %d.%d Parallel compute units: %d\n", i + 1, 4, maxComputeUnits);
+		}
+	}
+	int platformNum;
+	int deviceNum;
+	printf("\n\nSELECT PLATFORM('1' ~ '%d') : ", platformCount);
+	scanf("%d", &platformNum);
+	printf("\n");
+	printf("SELECT DEVICE('1' ~ '%d') : ", deviceCount);
+	scanf("%d", &deviceNum);
+	printf("\n");
+	clGetDeviceIDs(platforms[platformNum - 1], CL_DEVICE_TYPE_ALL, deviceCount, devices, NULL);
+
+	
+	cl_device_id device = devices[deviceNum - 1];
+
+	//create context
+	cl_context context = clCreateContext(NULL, 1, &device, NULL, NULL, NULL);
+
+	//create command queue
+	cl_command_queue queue = clCreateCommandQueue(context, device, 0, NULL);
+
+	// 텍스트파일로부터 프로그램 읽기
+	//char* source = readSource("convolution.cl");
+
+	// compile program
+	//cl_program program = clCreateProgramWithSource(context, 1,
+	//	(const char**)& source, NULL, NULL);
+	//cl_int build_status;
+	//build_status = clBuildProgram(program, 1, &device, NULL, NULL,
+	//	NULL);
+	//
+	////커널 포인터 생성
+	//auto simpleKernel = clCreateKernel(program, "simpleKernel", NULL);
+
+	return 0;
+}
