7e516d344b5e975397a0884241eede4455b166c7
welcometothegalaxy <iamjam4944@gmail.com>
2019-11-02 13:51:00 +0900

    add link boost

---
+++ framework/boost_proj/boost_tutorial.cpp
@@ -0,0 +1,13 @@
+//this is only for CMake tutorial
+// boost linking tutorial
+
+#include <iostream>
+#include "boost/optional.hpp"
+
+int main()
+{
+
+    std::cout<< "tutorial 2"<<std::endl;
+
+	return 0;
+}
